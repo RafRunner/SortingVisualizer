c680319b2174a4ac1d8b41358bdddcb5ecf6c6e8
Rafael Nunes <rafaelns.br@gmail.com>
2020-08-06 03:00:58 -0300

    Removing testing line

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,15 +1,10 @@
 package main;
 
-import domain.ArrayOperation;
-import enuns.ESortingAlgorithm;
 import view.MainWindow;
-
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<ArrayOperation> teste = ESortingAlgorithm.MergeSort.sortingSolution.getSortingSteps(new int[]{4,9,7,1});
         new MainWindow().showMenu();
     }
 }
